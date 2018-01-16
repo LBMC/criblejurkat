3fa30efe4c78e7acecd865b691a7c52ba6213f62
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-01-16 09:13:38 +0100

    everything to run unit testing for the pkg

---
+++ src/pkg/tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(CribleJurkat)
+
+test_check("CribleJurkat")
---
+++ src/pkg/tests/testthat/test_project.R
@@ -0,0 +1,12 @@
+library(CribleJurkat)
+context("Run CribleJurkat analysis")
+
+test_that("loading data", {
+  expect_identical(
+    paste0("gene", 1:100),
+    paste0("gene", 1:100))
+  expect_equal(
+    10:6,
+    10:6)
+)
+})
