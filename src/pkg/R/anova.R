301b9e374f49dc895c06d298a7c093640f5117c0
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-05-23 15:07:52 +0200

    anova.R: fix column and line variable creation

--- src/pkg/R/anova.R
+++ src/pkg/R/anova.R
@@ -3,8 +3,6 @@
 #' @param fcs_data an object of class flowSet
 #' @param channels (default = c("Y1.A", "B1.A)) the column ratio will be the
 #' ratio of Y1.A / B1.A
-#' @param line_lenght (default = 12) length of the lines in the plate
-#' @param column_length (default = 8) length of the columns in the plate
 #' @return a data.frame
 #' @examples
 #' \dontrun{
@@ -13,9 +11,7 @@
 #' @importFrom flowCore sampleNames pData phenoData
 #' @importFrom flowWorkspace GatingSet
 #' @export flowset2dataframe
-flowset2dataframe <- function(fsc_data, channels = c("Y1.A", "B1.A"),
-                              line_length = 12,
-                              column_length = 8) {
+flowset2dataframe <- function(fsc_data, channels = c("Y1.A", "B1.A")) {
   data <- fcs_data
   data <- apply(matrix(flowCore::sampleNames(data), ncol = 1), 1,
     FUN = function(x, fset, infos){
@@ -39,7 +35,7 @@ flowset2dataframe <- function(fsc_data, channels = c("Y1.A", "B1.A"),
   data$drug <- as.vector(data$drug)
   data <- compute_ratio(data, channels)
   data <- parse_drug(data)
-  data <- compute_line_column(data, line_lenght, column_length)
+  data <- compute_line_column(data)
   return(data)
 }
 
@@ -51,7 +47,7 @@ compute_ratio <- function(data, channels) {
 }
 
 parse_drug <- function(data) {
-  data$drug_status <- data$drug
+  data$drug_status <- as.factor(data$drug)
   b_drug <- !(data$drug %in% "None")
   data$drug[b_drug] <- paste0(as.vector(data$drug[b_drug]), "_",
                               as.vector(data$code.well[b_drug]))
@@ -60,10 +56,10 @@ parse_drug <- function(data) {
   return(data)
 }
 
-compute_line_column <- function(data, line_lenght = 12, column_length = 8) {
-  well_number <- as.numeric(as.factor(data$code.well))
-  data$line <- floor(( well_number - 1 ) / 12)
-  data$column <- floor(well_number / 12)
+compute_line_column <- function(data) {
+  well <- as.vector(data$code.well)
+  data$line <- gsub("([A-Z])[0-9]{2}", "\\1", well)
+  data$column <- gsub("[A-Z]([0-9]{2})", "\\1", well)
   return(data)
 }
 
