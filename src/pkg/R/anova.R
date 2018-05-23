895a4ee6fed7b4fadc98e08901df6a8c7e0460a4
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-05-23 11:08:58 +0200

    anova.R: add function to convert flowset to data.frame

---
+++ src/pkg/R/anova.R
@@ -0,0 +1,44 @@
+#' convert annotated flowSet to data.frame
+#'
+#' @param fcs_data an object of class flowSet
+#' @param channels (default = c("Y1.A", "B1.A)) the column ratio will be the
+#' ratio of Y1.A / B1.A
+#' @return a data.frame
+#' @examples
+#' \dontrun{
+#' data <- flowset2dataframe(fsc_data)
+#' }
+#' @importFrom flowCore sampleNames pData phenoData
+#' @importFrom flowWorkspace GatingSet
+#' @export rm_debris
+flowset2dataframe <- function(fsc_data, channels = c("Y1.A", "B1.A")) {
+  data <- fcs_data
+  data <- apply(matrix(flowCore::sampleNames(data), ncol = 1), 1,
+    FUN = function(x, fset, infos){
+      data <- data.frame(exprs(fset[[x]]))
+      infos <- infos[rownames(infos) %in% x, ]
+      data <- base::data.frame(
+        x,
+        1:nrow(data),
+        data,
+        infos,
+        row.names = NULL
+      )
+      return(data)
+    },
+    fset = data,
+    infos = flowCore::pData(flowCore::phenoData(fcs_data))
+  )
+  data <- data.frame(do.call(rbind, data))
+  names(data)[1:2] <- c("well", "step")
+  data$name <- as.factor(data$name)
+  b_ratio_1 <- colnames(data) %in% channels[1]
+  b_ratio_2 <- colnames(data) %in% channels[2]
+  data$ratio <- data[, b_ratio_1] / data[, b_ratio_2]
+  b_drug <- !(data$drug %in% "None")
+  data$drug[b_drug] <- paste0(data$drug[b_drug], data$well[b_drug])
+  data$drug <- as.factor((data$drug)
+  data$drug <- relevel(data$drug, "None")
+  return(data)
+}
+
