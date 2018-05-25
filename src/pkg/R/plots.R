b9c6a1c55e3a2751bd01d086f7c33f00f6f90f0c
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-05-25 10:36:18 +0200

    plots.R: add signif info on well plots

--- src/pkg/R/plots.R
+++ src/pkg/R/plots.R
@@ -19,13 +19,13 @@ plot_well <- function(data, sample_size = nrow(data) / 100, sufix = "") {
     s_data$mean_x <- mean_x[s_data$code.well]
     p <- ggplot()
     if ("signif" %in% colnames(s_data)) {
-      p <- geom_violin(alpha = 0, data = s_data,
-          aes(x = code.well, y = x, color = drug_status, fill = signif)) +
+      p <- p + geom_violin(alpha = 0, data = s_data,
+          aes(x = code.well, y = x, color = signif))
     } else {
-      p <- geom_violin(alpha = 0, data = s_data,
-          aes(x = code.well, y = x, color = drug_status)) +
+      p <- p + geom_violin(alpha = 0, data = s_data,
+          aes(x = code.well, y = x, color = drug_status))
     }
-    p <- geom_point(data = s_data,
+    p <- p + geom_point(data = s_data,
               aes(x = code.well, y = mean_x, color = drug_status)) +
       facet_wrap(~code.well, scale = "free_x", ncol = 12) +
       labs(y = x) +
