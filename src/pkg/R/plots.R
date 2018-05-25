48414a182362531890d2277d85bf68f4b5afc16e
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-05-25 09:20:16 +0200

    plot.R: fill well according to significance

--- src/pkg/R/plots.R
+++ src/pkg/R/plots.R
@@ -7,28 +7,36 @@
 #' @return a ggplot2 object
 #' @examples
 #' \dontrun{
-#' plot_plates_ratio(fsc_data)
+#' plot_plates(fsc_data)
 #' }
 #' @import ggplot2
-#' @export plot_well_ratio
-plot_well_ratio <- function(data, sample_size = nrow(data) / 100, sufix = "") {
+#' @export plot_well
+plot_well <- function(data, sample_size = nrow(data) / 100, sufix = "") {
   s_data <- data[sample(1:nrow(data), nrow(data)/100), ]
-  mean_ratio <- by(s_data$ratio, s_data$code.well, mean)
-  s_data$mean_ratio <- mean_ratio[s_data$code.well]
-  p <- ggplot(data = s_data,
-        aes(x = code.well, y = ratio, color = drug_status)) +
-    geom_violin(alpha = 0) +
-    geom_point(data = s_data,
-              aes(x = code.well, y = mean_ratio, color = drug_status)) +
-    facet_wrap(~code.well, scale = "free_x", ncol = 12) +
-    theme_bw()
-  print(p)
-  outdir <- mk_outdir(fcs_data, "summary")
-  ggplot2::ggsave(
-    filename = paste0(outdir, "well_ratio", sufix, ".pdf"), plot = p,
-    width = 29.7, height = 21, units = "cm", scale = 2
-  )
-  return(p)
+  for (x in c("ratio_norm", "Y1.A", "B1.A")) {
+    s_data$x <- s_data[[x]]
+    mean_x <- by(s_data$x, s_data$code.well, mean)
+    s_data$mean_x <- mean_x[s_data$code.well]
+    p <- ggplot()
+    if ("signif" %in% colnames(s_data)) {
+      p <- geom_violin(alpha = 0, data = s_data,
+          aes(x = code.well, y = x, color = drug_status, fill = signif)) +
+    } else {
+      p <- geom_violin(alpha = 0, data = s_data,
+          aes(x = code.well, y = x, color = drug_status)) +
+    }
+    p <- geom_point(data = s_data,
+              aes(x = code.well, y = mean_x, color = drug_status)) +
+      facet_wrap(~code.well, scale = "free_x", ncol = 12) +
+      labs(y = x) +
+      theme_bw()
+    print(p)
+    outdir <- mk_outdir(fcs_data, "summary")
+    ggplot2::ggsave(
+      filename = paste0(outdir, "well_", x, sufix, ".pdf"), plot = p,
+      width = 29.7, height = 21, units = "cm", scale = 2
+    )
+  }
 }
 
 #' plot the ratio distribution per column
@@ -43,26 +51,29 @@ plot_well_ratio <- function(data, sample_size = nrow(data) / 100, sufix = "") {
 #' plot_column_ratio(fsc_data)
 #' }
 #' @import ggplot2
-#' @export plot_well_ratio
-plot_column_ratio <- function(data, sample_size = nrow(data) / 100,
+#' @export plot_column
+plot_column <- function(data, sample_size = nrow(data) / 100,
                               sufix = "") {
   s_data <- data[sample(1:nrow(data), nrow(data)/100), ]
-  mean_ratio <- by(s_data$ratio, s_data$column, mean)
-  s_data$mean_ratio <- mean_ratio[s_data$column]
-  p <- ggplot(data = s_data,
-        aes(x = column, y = ratio)) +
-    geom_violin(alpha = 0) +
-    geom_point(data = s_data,
-              aes(x = column, y = mean_ratio)) +
-    facet_wrap(~column, scale = "free_x", ncol = 12) +
-    theme_bw()
-  print(p)
-  outdir <- mk_outdir(fcs_data, "summary")
-  ggplot2::ggsave(
-    filename = paste0(outdir, "column_ratio", sufix, ".pdf"), plot = p,
-    width = 29.7, height = 11, units = "cm", scale = 2
-  )
-  return(p)
+  for (x in c("ratio_norm", "Y1.A", "B1.A")) {
+    s_data$x <- s_data[[x]]
+    mean_x <- by(s_data$x, s_data$column, mean)
+    s_data$mean_x <- mean_x[s_data$column]
+    p <- ggplot(data = s_data,
+          aes(x = column, y = x)) +
+      geom_violin(alpha = 0) +
+      geom_point(data = s_data,
+                aes(x = column, y = mean_x)) +
+      facet_wrap(~column, scale = "free_x", ncol = 12) +
+      labs(y = x) +
+      theme_bw()
+    print(p)
+    outdir <- mk_outdir(fcs_data, "summary")
+    ggplot2::ggsave(
+      filename = paste0(outdir, "column_", x, sufix, ".pdf"), plot = p,
+      width = 29.7, height = 11, units = "cm", scale = 2
+    )
+  }
 }
 
 #' plot the ratio distribution per line
@@ -77,23 +88,26 @@ plot_column_ratio <- function(data, sample_size = nrow(data) / 100,
 #' plot_line_ratio(fsc_data)
 #' }
 #' @import ggplot2
-#' @export plot_well_ratio
-plot_line_ratio <- function(data, sample_size = nrow(data) / 100, sufix = "") {
+#' @export plot_line
+plot_line <- function(data, sample_size = nrow(data) / 100, sufix = "") {
   s_data <- data[sample(1:nrow(data), nrow(data)/100), ]
-  mean_ratio <- by(s_data$ratio, s_data$line, mean)
-  s_data$mean_ratio <- mean_ratio[s_data$line]
-  p <- ggplot(data = s_data,
-        aes(x = line, y = ratio)) +
-    geom_violin(alpha = 0) +
-    geom_point(data = s_data,
-              aes(x = line, y = mean_ratio)) +
-    facet_wrap(~line, scale = "free_x", ncol = 8) +
-    theme_bw()
-  print(p)
-  outdir <- mk_outdir(fcs_data, "summary")
-  ggplot2::ggsave(
-    filename = paste0(outdir, "line_ratio", sufix, ".pdf"), plot = p,
-    width = 29.7, height = 11, units = "cm", scale = 2
-  )
-  return(p)
+  for (x in c("ratio_norm", "Y1.A", "B1.A")) {
+    s_data$x <- s_data[[x]]
+    mean_x <- by(s_data$x, s_data$line, mean)
+    s_data$mean_x <- mean_x[s_data$line]
+    p <- ggplot(data = s_data,
+          aes(x = line, y = x)) +
+      geom_violin(alpha = 0) +
+      geom_point(data = s_data,
+                aes(x = line, y = mean_x)) +
+      facet_wrap(~line, scale = "free_x", ncol = 8) +
+      labs(y = x)
+      theme_bw()
+    print(p)
+    outdir <- mk_outdir(fcs_data, "summary")
+    ggplot2::ggsave(
+      filename = paste0(outdir, "line_", x, sufix, ".pdf"), plot = p,
+      width = 29.7, height = 11, units = "cm", scale = 2
+    )
+  }
 }
