9d41c1bca67b6928a99eb3fc77dc254b758056d6
Laurent Modolo <laurent@modolo.fr>
2018-10-19 16:19:26 +0200

    project.R: fix missing {

--- R/project.R
+++ R/project.R
@@ -69,7 +69,7 @@ analysis <- function(data_path = "data/") {
     sets_list[[folder]] <- set_analysis(paste0(data_path, "/", folder),
                                         meta = T)
     sets_list[[folder]]$set <- folder
-    if (min_sets_length < 0 | length(sets_list[[folder]]) < min_sets_length )
+    if (min_sets_length < 0 | length(sets_list[[folder]]) < min_sets_length ) {
       min_sets_length <- length(sets_list[[folder]])
       min_sets_factors <- colnames(sets_list[[folder]])
     }
