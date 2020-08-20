f27e633f5e8780c19797ba3bd0793c662fe8711f
Laurent Modolo <laurent@modolo.fr>
2020-08-20 14:26:00 +0200

    gating.R: update rm_outliers to match new packages version

--- R/gating.R
+++ R/gating.R
@@ -84,22 +84,24 @@ rm_debris <- function(fcs_data) {
 rm_nonsinglets <- function(fcs_data) {
   message("removing non-singlets")
   outdir <- mk_outdir(fcs_data, "gating")
-  full_sample_names <- flowWorkspace::sampleNames(fcs_data)
-  flowWorkspace::sampleNames(fcs_data) <- gsub(
-    ".*/([^/]*)", "\\1", full_sample_names
-  )
   bi_expTrans <- flowWorkspace::flowjo_biexp_trans()
   tl <- flowCore::transformList(
     c("Y1.A", "B1.A"), list(bi_expTrans$transform, bi_expTrans$transform)
   )
-  wf <- flowCore::transform(fcs_data, tl)
-  wf <- flowWorkspace::GatingSet(wf)
+  fcs_singlets <- fcs_data
+  flowWorkspace::sampleNames(fcs_singlets) <- gsub(
+    ".*/([^/]*)", "\\1", flowWorkspace::sampleNames(fcs_singlets)
+  )
+  fcs_singlets <- flowWorkspace::flowSet_to_cytoset(fcs_singlets)
+  fcs_singlets <- flowCore::transform(fcs_singlets, tl)
+  fcs_singlets <- flowWorkspace::GatingSet(fcs_singlets)
   openCyto::gs_add_gating_method(
-    wf, alias = "singlets", pop = "+", parent = "root",
+    fcs_singlets, alias = "singlets", pop = "+", parent = "root",
     dims = "FSC.A,FSC.H", gating_method = "singletGate",
     gating_args = "wider_gate=TRUE, maxit = 100"
   )
-  p <- ggcyto::ggcyto(wf, mapping = ggplot2::aes(x = FSC.A,y = FSC.H)) +
+  p <- ggcyto::ggcyto(fcs_singlets,
+                      mapping = ggplot2::aes(x = FSC.A,y = FSC.H)) +
     ggplot2::geom_hex(bins = 50) +
     ggcyto::geom_gate("singlets") +
     ggcyto::ggcyto_par_set(limits = "instrument") +
@@ -109,8 +111,11 @@ rm_nonsinglets <- function(fcs_data) {
     filename = paste0(outdir, "singlets.pdf"), plot = p,
     width = 29.7, height = 21, units = "cm", scale = 2
   )
-  fcs_singlets <- flowWorkspace::gs_pop_get_data(wf, "singlets")
-  flowWorkspace::sampleNames(fcs_singlets) <- full_sample_names 
+  fcs_singlets <- flowWorkspace::gs_pop_get_data(fcs_singlets, "singlets")
+  fcs_singlets <- flowWorkspace::cytoset_to_flowSet(fcs_singlets)
+  flowWorkspace::sampleNames(fcs_singlets) <- flowWorkspace::sampleNames(
+    fcs_data
+  )
   return(fcs_singlets)
 }
 
@@ -130,23 +135,20 @@ rm_nonsinglets <- function(fcs_data) {
 rm_nonfluo <- function(fcs_data) {
   message("removing outliers")
   outdir <- mk_outdir(fcs_data, "gating")
-  fcs_fluo <- fcs_data
+  fcs_fluo <- fcs_data[seq(along = fcs_data)]
   grDevices::pdf(paste0(outdir, "fluo.pdf"), width = 29,7, height = 21)
   graphics::par(mfrow = c(8, 12))
   pb <- utils::txtProgressBar(min = 0, max = length(fcs_data),
                              initial = 1, style = 3)
-  print(length(fcs_data))
-  print(ncol(fcs_data))
-  print(nrow(fcs_data))
   for (i in 1:length(fcs_data)) {
-    # suppressMessages(
-      res <- flowClust::flowClust(
+    res <- suppressMessages(
+      flowClust::flowClust(
         fcs_data[[i]],
         varNames = c("Y1.A", "B1.A"),
         K = 1,
         B = 100
       )
-    # )
+    )
     utils::capture.output(
       flowClust::plot(
         res,
@@ -155,19 +157,19 @@ rm_nonfluo <- function(fcs_data) {
         z.cutoff = 0
       )
     )
-    fcs_fluo[[i]] <- flowClust::split(
+    res <- flowClust::split(
       fcs_data[[i]],
       res,
       rm.outliers = TRUE,
       population = list(
         fluo = 1
       )
-    )$fluo
+    )
+    fcs_fluo[[i]] <- res$fluo
     utils::setTxtProgressBar(pb, i)
   }
   close(pb)
   grDevices::dev.off()
   return(fcs_fluo)
 }
-
 
