24f2ba4a7f38162aa38ea488e6a7e107f9b2bda9
Laurent Modolo <laurent@modolo.fr>
2020-09-07 09:48:40 +0200

    add option in container script

--- docker_analysis.sh
+++ docker_analysis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-R_CMD="-e criblejurkat::analysis(\"/root/\")"
+R_CMD="-e criblejurkat::analysis(\"/root/${1}\")"
 docker run -it --volume $(pwd):/root/ lbmc/criblejurkat:1.0.0 R $R_CMD
 BASH_CMD="-e system(\"chown -R $(id -u ${USER}) /root/\")"
 docker run -it --volume $(pwd):/root/ lbmc/criblejurkat:1.0.0 R $BASH_CMD
--- singularity_analysis.sh
+++ singularity_analysis.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-R_CMD="-e criblejurkat::analysis(\"$(pwd)\")"
+R_CMD="-e criblejurkat::analysis(\"${1}\")"
 singularity exec docker://lbmc/criblejurkat:1.0.0 R $R_CMD
